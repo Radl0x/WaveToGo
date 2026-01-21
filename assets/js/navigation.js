d1c765ce42ada002924fd075b6ed173c4f58bb2f
Radl0x <lucabassu01@gmail.com>
2026-01-21 20:48:45 +0100

    s

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -14,3 +14,15 @@ items.forEach(item => observer.observe(item));
 
 
 
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  const tabPanels = document.querySelectorAll(".tab-panel");
+
+  tabButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      tabButtons.forEach(b => b.classList.remove("active"));
+      tabPanels.forEach(p => p.classList.remove("active"));
+
+      btn.classList.add("active");
+      document.getElementById(btn.dataset.tab).classList.add("active");
+    });
+  });
